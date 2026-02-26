d4300f70029df9c3a24346d9edd75f29d0c4d7cf
Hopeail <hopeail@github.com>
2026-02-26 09:12:54 +0000

    Fix: Add db:migrate protection for Discourse 2026.2.0 compatibility
    - Wrap after_initialize in Rake task check to prevent loading during database migrations
    - Add reference link to Discourse plugin development docs
    - Resolve bootstrap failure: Pups::ExecError db:migrate exit 1
    
    Fixes: discourse-setup app rebuild failure during db:migrate phase

--- plugin.rb
+++ plugin.rb
@@ -6,103 +6,104 @@
 
 enabled_site_setting :encrypted_api_enabled
 
-# Discourse 插件标准加载方式
-# 控制器会在 after_initialize 时自动加载
+# Discourse 2026.2.0 兼容性：避免 db:migrate 阶段加载插件
+# 参考：https://docs.discourse.org/developing-plugins/
+unless defined?(Rake) && Rake.application.top_tasks.include?('db:migrate')
+  after_initialize do
+    # 延迟加载控制器（Discourse 2026.2.0 标准方式）
+    module ::EncryptedApi
+      class RsaEncryptor
+        require 'openssl'
+        require 'base64'
+        require 'json'
 
-after_initialize do
-  # 延迟加载控制器（避免 db:migrate 时出错）
-  module ::EncryptedApi
-    class RsaEncryptor
-      require 'openssl'
-      require 'base64'
-      require 'json'
+        KEY_LENGTH = 2048
 
-      KEY_LENGTH = 2048
+        class << self
+          def generate_keypair
+            key = OpenSSL::PKey::RSA.new(KEY_LENGTH)
+            { public_key: key.public_key.to_pem, private_key: key.to_pem }
+          end
 
-      class << self
-        def generate_keypair
-          key = OpenSSL::PKey::RSA.new(KEY_LENGTH)
-          { public_key: key.public_key.to_pem, private_key: key.to_pem }
-        end
+          def load_private_key(pem_string)
+            OpenSSL::PKey::RSA.new(pem_string)
+          end
 
-        def load_private_key(pem_string)
-          OpenSSL::PKey::RSA.new(pem_string)
-        end
+          def load_public_key(pem_string)
+            OpenSSL::PKey::RSA.new(pem_string)
+          end
 
-        def load_public_key(pem_string)
-          OpenSSL::PKey::RSA.new(pem_string)
-        end
+          def sign(data, private_key_pem)
+            private_key = load_private_key(private_key_pem)
+            signature = private_key.sign(OpenSSL::Digest::SHA256.new, data.to_json)
+            Base64.strict_encode64(signature)
+          end
 
-        def sign(data, private_key_pem)
-          private_key = load_private_key(private_key_pem)
-          signature = private_key.sign(OpenSSL::Digest::SHA256.new, data.to_json)
-          Base64.strict_encode64(signature)
-        end
+          def verify(data, signature_base64, public_key_pem)
+            public_key = load_public_key(public_key_pem)
+            signature = Base64.strict_decode64(signature_base64)
+            public_key.verify(OpenSSL::Digest::SHA256.new, signature, data.to_json)
+          end
 
-        def verify(data, signature_base64, public_key_pem)
-          public_key = load_public_key(public_key_pem)
-          signature = Base64.strict_decode64(signature_base64)
-          public_key.verify(OpenSSL::Digest::SHA256.new, signature, data.to_json)
-        end
+          def encrypt(data, public_key_pem)
+            public_key = load_public_key(public_key_pem)
+            encrypted = public_key.public_encrypt(data.to_json)
+            Base64.strict_encode64(encrypted)
+          end
 
-        def encrypt(data, public_key_pem)
-          public_key = load_public_key(public_key_pem)
-          encrypted = public_key.public_encrypt(data.to_json)
-          Base64.strict_encode64(encrypted)
-        end
+          def decrypt(encrypted_base64, private_key_pem)
+            private_key = load_private_key(private_key_pem)
+            encrypted = Base64.strict_decode64(encrypted_base64)
+            JSON.parse(private_key.private_decrypt(encrypted))
+          end
 
-        def decrypt(encrypted_base64, private_key_pem)
-          private_key = load_private_key(private_key_pem)
-          encrypted = Base64.strict_decode64(encrypted_base64)
-          JSON.parse(private_key.private_decrypt(encrypted))
-        end
+          def current_private_key
+            pem = SiteSetting.encrypted_api_private_key
+            raise 'Private key not configured' if pem.blank?
+            pem
+          end
 
-        def current_private_key
-          pem = SiteSetting.encrypted_api_private_key
-          raise 'Private key not configured' if pem.blank?
-          pem
-        end
+          def current_public_key
+            pem = SiteSetting.encrypted_api_public_key
+            raise 'Public key not configured' if pem.blank?
+            pem
+          end
 
-        def current_public_key
-          pem = SiteSetting.encrypted_api_public_key
-          raise 'Public key not configured' if pem.blank?
-          pem
-        end
-
-        def encrypt_response(data)
-          timestamp = Time.now.utc.iso8601
-          payload = { data: data, timestamp: timestamp }
-          signature = sign(payload, current_private_key)
-          encrypted_data = encrypt(payload, current_public_key)
-          { encrypted: encrypted_data, signature: signature, algorithm: 'RSA-OAEP-SHA256', timestamp: timestamp }
+          def encrypt_response(data)
+            timestamp = Time.now.utc.iso8601
+            payload = { data: data, timestamp: timestamp }
+            signature = sign(payload, current_private_key)
+            encrypted_data = encrypt(payload, current_public_key)
+            { encrypted: encrypted_data, signature: signature, algorithm: 'RSA-OAEP-SHA256', timestamp: timestamp }
+          end
         end
       end
     end
-  end
 
-  # 注册 API 路由
-  add_api_route :get, '/encrypted/session/current' do
-    before { ensure_logged_in }
-    action { EncryptedApiController.current_session }
-  end
+    # 注册 API 路由（Discourse 2026.2.0 兼容）
+    add_api_route :get, '/encrypted/session/current' do
+      before { ensure_logged_in }
+      action { EncryptedApiController.current_session }
+    end
 
-  add_api_route :get, '/encrypted/user/:username/summary' do
-    before { ensure_logged_in }
-    action { EncryptedApiController.user_summary }
-  end
+    add_api_route :get, '/encrypted/user/:username/summary' do
+      before { ensure_logged_in }
+      action { EncryptedApiController.user_summary }
+    end
 
-  add_api_route :get, '/encrypted/session/:username' do
-    before { ensure_logged_in }
-    action { EncryptedApiController.session_username }
-  end
+    add_api_route :get, '/encrypted/session/:username' do
+      before { ensure_logged_in }
+      action { EncryptedApiController.session_username }
+    end
 
-  add_api_route :post, '/admin/encrypted/keys/generate' do
-    before { ensure_is_admin }
-    action { EncryptedApiController.generate_keys }
-  end
+    add_api_route :post, '/admin/encrypted/keys/generate' do
+      before { ensure_is_admin }
+      action { EncryptedApiController.generate_keys }
+    end
 
-  add_api_route :get, '/admin/encrypted/keys/public' do
-    before { ensure_logged_in }
-    action { EncryptedApiController.get_public_key }
+    add_api_route :get, '/admin/encrypted/keys/public' do
+      before { ensure_logged_in }
+      action { EncryptedApiController.get_public_key }
+    end
   end
 end
